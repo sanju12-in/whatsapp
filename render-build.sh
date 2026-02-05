4fcb727ec945b9ce0a872394e3e5f83ff757d2e7
sanju12-in <57477523+sanju12-in@users.noreply.github.com>
2026-02-06 02:26:46 +0530

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
-# Exit on error
 set -o errexit
 
 npm install
-# Force Puppeteer to download Chrome
 npx puppeteer browsers install chrome
+echo "Build Completed Successfully!"
