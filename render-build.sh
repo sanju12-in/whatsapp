dbc153fe077368f90496901214f6c2fa1ca92c5e
sanju12-in <57477523+sanju12-in@users.noreply.github.com>
2026-02-06 02:38:57 +0530

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
+# Exit immediately if a command exits with a non-zero status
 set -o errexit
 
+echo "--- STARTING BUILD ---"
+
+echo "1. Installing NPM Dependencies..."
 npm install
+
+echo "2. Downloading Chrome for Puppeteer..."
 npx puppeteer browsers install chrome
-echo "Build Completed Successfully!"
+
+echo "--- BUILD COMPLETED SUCCESSFULLY ---"
